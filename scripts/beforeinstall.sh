b5d20d8728d17cc4143e582f3a2a11239fcbd2e1
Amityadavv <69285585+Amityadavv@users.noreply.github.com>
2020-11-26 18:16:36 +0530

    Update beforeinstall.sh

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-sudo apt-get update -y
+ apt-get update -y
 
-sudo apt install default-jre
+ apt install default-jre
 
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
-sudo apt-get install -y nodejs
+apt-get install -y nodejs
 
-sudo npm install -g pm2
+npm install -g pm2
