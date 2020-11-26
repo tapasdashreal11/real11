e8d4161e0533c262260bfe3cd523264959aab264
Amityadavv <69285585+Amityadavv@users.noreply.github.com>
2020-11-26 17:26:16 +0530

    Update installpkgs.sh

--- scripts/installpkgs.sh
+++ scripts/installpkgs.sh
@@ -2,5 +2,15 @@
 
 cd /home/ubuntu/node 
 
+sudo apt-get update -y
+
+sudo apt install default-jre
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+
+sudo apt-get install -y nodejs
+
+sudo npm install -g pm2
+
 sudo npm install
 
