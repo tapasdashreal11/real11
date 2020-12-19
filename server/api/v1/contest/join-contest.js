be29a88f81c0df9fa4771300b5da9d379cef46c9
nidhimittal16 <65557308+nidhimittal16@users.noreply.github.com>
2020-12-19 13:13:12 +0530

    redis work

--- server/api/v1/contest/join-contest.js
+++ server/api/v1/contest/join-contest.js
@@ -371,13 +371,7 @@ module.exports = async (req, res) => {
                                                                             await UserAnalysis.updateOne({ _id: ObjectId(userBounousData._id) }, { $inc: { "offer_percent": -percent } });
                                                                             console.log('redisKeyForRentation**',redisKeyForRentation);
                                                                             //redis.userAnalysisRedisObj.del(redisKeyForRentation);
-                                                                            await redis.userAnalysisRedisObj.del(redisKeyForRentation, function(err, response) {
-                                                                                if (response == 1) {
-                                                                                   console.log("Redis Deleted Successfully!");
-                                                                                } else {
-                                                                                 console.log("Redis Cannot delete***",err);
-                                                                                }
-                                                                             });
+                                                                            redis.deleteRedisFavouriteContest(redisKeyForRentation);
                                                                              
                                                                         }
                                                                         
