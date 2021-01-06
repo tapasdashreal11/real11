4b7a67648109dc87ee55c4b600033e6e823e1d49
shashireal11 <>
2021-01-06 13:19:13 +0530

    Update retantion-data-scrip.js

--- server/api/v1/users/retantion-data-scrip.js
+++ server/api/v1/users/retantion-data-scrip.js
@@ -2,34 +2,45 @@ const { ObjectId } = require('mongodb');
 const _ = require("lodash");
 const UserAnalysis = require("../../../models/user-analysis");
 const FavouriteContest = require("../../../models/favourite-contest");
-
+const redis = require('../../../../lib/redis');
 module.exports = {
   favouriteContestCreate: async (req, res) => {
     try {
       var response = { status: false, message: "Invalid Request", data: {} };
-
+       console.log('test***');
       try {
         let {
             bulkdata
         } = req.body
 
-
+        console.log('test***1');
         if(bulkdata && _.isArray(bulkdata) && bulkdata.length>0){
-            if(bulkdata &&  bulkdata.contest_data && bulkdata.contest_data.length){
-                for (const cData of bulkdata.contest_data) {
-                    cData.contest_id = ObjectId(cData.contest_id)
+            if(bulkdata){
+                for (const item of bulkdata) {
+                    for (const cData of item.contest_data) {
+                        cData.contest_id = ObjectId(cData.contest_id)
+                    }
                 }
+                
+                FavouriteContest.insertMany(bulkdata)
+                .then(function(mongooseDocuments) {
+                    console.log('test***3');
+                    console.log(mongooseDocuments);
+                    for (const resItem of mongooseDocuments) {
+                        let redisKeyForFavouriteContest = 'favourite-contest-' + resItem.user_id;
+                        redis.setRedisFavouriteContest(redisKeyForFavouriteContest, resItem);
+                    }
+                    
+                    response['data'] = mongooseDocuments;
+                    return res.json(response);
+                })
+                .catch(function(err) {
+                    /* Error handling */
+                    console.log('test***4');
+                    return res.json(response);
+                });
             }
-            FavouriteContest.insertMany(bulkdata)
-            .then(function(mongooseDocuments) {
-                console.log(mongooseDocuments);
-                response['data'] = mongooseDocuments;
-                return res.json(response);
-            })
-            .catch(function(err) {
-                /* Error handling */
-                return res.json(response);
-            });
+           
 
         }
         
