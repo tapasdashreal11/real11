ce6f716cfb81840a33579daf7ea2f27f23621c56
shashireal11 <>
2021-01-23 15:13:15 +0530

    Update contest-list.js

--- server/api/v1/contest/contest-list.js
+++ server/api/v1/contest/contest-list.js
@@ -101,7 +101,7 @@ module.exports = async (req, res) => {
                     try{
                          newMatchContestData = _.reject(newMatchContestData, function(e) {
                              if(userCategory)console.log('e*********',e.category_id,userCategory.is_beginner_user,userCategory.is_super_user);
-                            return (ObjectId(e.category_id).equals(ObjectId('600a7d84a3d2553aa779eae7')) && userCategory && userCategory.is_beginner_user == 0 ) &&
+                            return (ObjectId(e.category_id).equals(ObjectId('600a7d84a3d2553aa779eae7')) && userCategory && userCategory.is_beginner_user == 0 ) ||
                             (ObjectId(e.category_id).equals(ObjectId('600a7dfaa3d2553aa779eae8')) && userCategory && userCategory.is_super_user == 0 )
                         });
                     }catch(eerrrr){}
