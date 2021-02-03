f6c8a4fd649fca67004a5d811ac812af1f2350e6
shashireal11 <>
2021-02-03 18:08:41 +0530

    Update notification.js

--- server/models/notification.js
+++ server/models/notification.js
@@ -5,6 +5,29 @@ const mongoose = require('mongoose');
 // {"id":"1","user_id":"33424","nitification_type":"3","unique_string":"",
 // "title":"Transaction","notification":"Your transaction is successful.",
 // "match_data":"","date":"2019-06-18","status":"1","is_send":"1"}
+
+
+/*const AdminSchema = mongoose.Schema({
+  nitification_type: String,
+  unique_string: String,
+  user_id: String,
+ 
+  title: String,
+  notification: String,
+  match_data: String,
+  date: String,  
+  is_send: { type: String, enum: [1,0] },
+  status: { type: String, enum: [1,0] }  
+    
+}, {
+        timestamps: { createdAt: 'created', updatedAt: 'modified' },
+        toObject: { getters: true, setters: true },
+        toJSON: { getters: true, setters: true }
+    }
+);*/
+
+// AdminSchema.plugin(mongoosePaginate);
+//module.exports = mongoose.model('Notification', AdminSchema);
 var Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
@@ -28,6 +51,4 @@ const AdminSchema = mongoose.Schema({
         toJSON: { getters: true, setters: true }
     }
 );
-
-// AdminSchema.plugin(mongoosePaginate);
 module.exports = mongoose.model('Notification', AdminSchema,'notifications');
