4482d3b2660231147a46986d8119b6e233a8e2ca
nidhimittal16 <65557308+nidhimittal16@users.noreply.github.com>
2021-02-16 14:36:32 +0530

    Update withdraw-request.js

--- server/api/v1/users/withdraw-request.js
+++ server/api/v1/users/withdraw-request.js
@@ -398,7 +398,7 @@ async function withdrawStatus(orderId, merchant_key, mid, cb) {
 			};
 			// console.log('enter');return false;
 			var response = "";
-			// setTimeout(function(){
+			setTimeout(function(){
 				var post_req = https.request(options, function (post_res) {
 					post_res.on('data', function (chunk) {
 						response += chunk;
@@ -419,7 +419,7 @@ async function withdrawStatus(orderId, merchant_key, mid, cb) {
 				});
 				post_req.write(post_data);
 				post_req.end();
-			// },4000)
+			},2000)
 		});
 	} catch (error) {
 		logger.error("ERROR", error.message);
