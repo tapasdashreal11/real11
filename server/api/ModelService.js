5604adb936df0421783bbbef36cff2b7267aeea8
shashireal11 <>
2021-04-02 20:48:52 +0530

    LF

--- server/api/ModelService.js
+++ server/api/ModelService.js
@@ -975,7 +975,7 @@ class ModelService {
             try {
                 this.collection.aggregate([
                     {
-                        $match:{over_match:true,live_match_close:0,status:1,match_status:"In Progress",sport:sport}
+                        $match:{over_match:true,status:1,match_status:"In Progress",sport:sport}
                     },
                     { $sort : {sort: -1, time : 1} },
                     { $limit : 100 },
