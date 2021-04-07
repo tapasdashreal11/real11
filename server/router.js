fddddc41913cb1831750de9140610dfb6e1be94f
nidhimittal16 <65557308+nidhimittal16@users.noreply.github.com>
2021-04-07 16:53:28 +0530

    Update router.js

--- server/router.js
+++ server/router.js
@@ -350,7 +350,7 @@ router.post('/cron/bizwebhook', function(req, res) {
 
 router.post('/phonePe/phonePewebhook', function(req, res) {
     console.log("phonePe callback data", req.body)
-    console.log("phonePe callback x-very string", req.headers["X-VERIFY"]);
+    console.log("phonePe callback x-very string", req.headers);
     const buff = Buffer.from(req.body.response, 'base64');
     const response = JSON.parse(buff.toString('utf-8'));
     
