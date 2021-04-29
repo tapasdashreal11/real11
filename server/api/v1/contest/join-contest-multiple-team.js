c4fedf61a84861ef8c6788cc8963a46fc2c70de9
shashireal11 <>
2021-04-29 15:46:01 +0530

    JC multiple team

--- server/api/v1/contest/join-contest-multiple-team.js
+++ server/api/v1/contest/join-contest-multiple-team.js
@@ -339,6 +339,15 @@ module.exports = async (req, res) => {
                                                                                 total_team_joined:totalTeamJoinedCount,
                                                                                 added_type: parseInt(status)
                                                                             };
+                                                                            let userBalance = await User.findById(user_id).select({ "winning_balance": 1, "cash_balance": 1, "bonus_amount": 1, "extra_amount": 1})
+                                                                            if(userBalance){
+                                                                                if(userBalance.extra_amount <= extraAmount || userBalance.cash_balance <= cashAmount || userBalance.winning_balance <= winAmount || userBalance.bonus_amount <= bonusAmount){
+                                                                                    userWalletStatus = false;
+                                                                                    await session.abortTransaction();
+                                                                                    session.endSession();
+                                                                                    return res.send(ApiUtility.failed("Please try again."));
+                                                                                }
+                                                                            }
 
                                                                             let walletRes = await User.updateOne({ _id: user_id }, { $set: updateUserData, $inc: { cash_balance: -cashAmount, bonus_amount: -bonusAmount, winning_balance: -winAmount, extra_amount: -extraAmount } }, sessionOpts);
 
