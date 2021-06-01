9094b2bd0ef9d1e64876a12c8082f2cf3cbacf7e
shashireal11 <>
2021-06-01 20:01:00 +0530

    Update ModelService.js

--- server/api/ModelService.js
+++ server/api/ModelService.js
@@ -1674,6 +1674,7 @@ class ModelService {
     myContestModel(skip, limit, sort, filter, sport, type){
         return new Promise(async (resolve, reject) => {
             try{
+                console.log("my match call from service*****");
                 var serverTime2 = moment(new Date()).format(config.DateFormat.datetime);
                 var matchesFilter = []
                 let sortTime =  {};
