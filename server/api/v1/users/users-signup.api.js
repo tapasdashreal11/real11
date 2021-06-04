1dc7e3d108163bf5ddf9cc1949351159a00d7d03
shashireal11 <>
2021-06-04 19:10:26 +0530

    Update users-signup.api.js

--- server/api/v1/users/users-signup.api.js
+++ server/api/v1/users/users-signup.api.js
@@ -237,7 +237,7 @@ module.exports = async (req, res) => {
             };
             console.log("signUpBody***",signUpBody);
          var resData = await appsFlyerEntryService(signUpBody,appsflyerURL);
-         console.log("apps flyer res",resData);
+         console.log("apps flyer res signup",resData);
           } catch(errr){
          console.log('errr',errr);
           }
