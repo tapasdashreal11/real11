ad262515867bb5d0b70b21f7ef5856ce14d07482
shashireal11 <>
2021-06-16 11:57:26 +0530

    Create other_game.js

---
+++ server/models/other_game.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema,
+	ObjectId = Schema.ObjectId;
+
+var otherGameSchema = new Schema({
+	id: {
+		type: String
+	},
+	match_id: {
+		type: Number
+	},
+	sport: {
+		type: Number
+	},
+	status: { type: Number, enum: [1, 0], default: 1 },
+	sort: {
+		type: Number
+	},
+	match_name: {
+		type: String
+	}
+}, {
+	timestamps: true
+});
+
+module.exports = mongoose.model('other_games', otherGameSchema, 'other_games');
