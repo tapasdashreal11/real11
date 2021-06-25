827e31f304322ff7e5adf5c58603b65f54da1a5c
shashireal11 <>
2021-06-25 16:21:42 +0530

    appsflyer Data

--- server/api/v1/users/appsflyer-api.js
+++ server/api/v1/users/appsflyer-api.js
@@ -5,15 +5,15 @@ const config = require('../../../config');
 
 const appsFlyerEntryService = (appsflyerdata,appsflyerUrl) => {
     return new Promise((resolve, reject) => {
-        const ddd = JSON.parse(JSON.stringify(appsflyerdata)) 
-        console.log('ddd****',ddd);
+         
         if(!isEmpty(appsflyerdata)){
+            const bData = JSON.parse(JSON.stringify(appsflyerdata));
             var options = {
                 "method": "POST",
                 "url": appsflyerUrl,
                 "json": true,
                 "headers": {'authentication':"BUvaUbjej7PNorfc5Kmpzg",'Content-Type': 'application/json'},
-                "body":ddd
+                "body":bData
             };
             request(options, function (error,res,body) {
                 console.log("**** applyer",body);
