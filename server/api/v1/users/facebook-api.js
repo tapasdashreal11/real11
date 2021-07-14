4e4eb2ad4cbb628040dfb5d2eb1b07b2eb040640
shashireal11 <>
2021-07-14 10:46:57 +0530

    remove console..

--- server/api/v1/users/facebook-api.js
+++ server/api/v1/users/facebook-api.js
@@ -5,9 +5,7 @@ const request = require('request');
 const facebookEntryService = (fbData,fbUrl) => {
     return new Promise((resolve, reject) => {
         if(!isEmpty(fbData)){
-            
             const bData = JSON.parse(JSON.stringify(fbData));
-            console.log(bData);
             var options = {
                 "method": "POST",
                 "url": "https://graph.facebook.com/v11.0/560345878459615/events?access_token=EAAHLiM2ETPEBAPK53iQgv6l5AIbZBoMkulrqUsai2StS32TafDN2Kr3mq8ZCktLvPNFK12tgZAjPHZAGC6ZAbTcHCUho5ZCTu6TtP3iZAbRqEesXuxw9kEZAkzsFCwHPESBbaGSZBQ07sf2bSgbNrnq7Dj1k54VbPYMZBuhodbtzQvoEOU4FCUe9PPIhi0pwZAJQvYZD", 
@@ -16,7 +14,6 @@ const facebookEntryService = (fbData,fbUrl) => {
                 "body":bData
             };
             request(options, function (error,res,body) {
-                console.log('fb res**',body);
                 return resolve(body);
             });
            
