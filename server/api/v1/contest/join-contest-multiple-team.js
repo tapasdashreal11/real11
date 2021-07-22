d883e5aeee443ebabc901d57f6e363a94dea8c09
shashireal11 <>
2021-07-22 11:42:35 +0530

    Update join-contest-multiple-team.js

--- server/api/v1/contest/join-contest-multiple-team.js
+++ server/api/v1/contest/join-contest-multiple-team.js
@@ -234,7 +234,7 @@ module.exports = async (req, res) => {
                                                                         let cGap = cSaleData.coupon_credit - cSaleData.coupon_used;
                                                                         if(cGap ==1 || totalCouponsToBeUsed == cGap ){
                                                                             console.log('data***11',cSaleData);
-                                                                            let ddd = await CouponSale.updateOne({ _id:ObjectId(cSaleData._id)},{$inc:{coupon_used:2}});
+                                                                            let ddd = await CouponSale.updateOne({ _id:ObjectId(cSaleData._id)},{ $inc :{ coupon_used: 2}});
                                                                             // redis.redisObj.set('my-coupons-' + user_id, JSON.stringify({}));
                                                                             console.log('ddd***11',ddd);
                                                                         } else {
