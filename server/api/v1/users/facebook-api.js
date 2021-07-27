719d6fe5032e2d72fbe475805ac13402899fe9ad
shashireal11 <>
2021-07-27 12:03:18 +0530

    Update facebook-api.js

--- server/api/v1/users/facebook-api.js
+++ server/api/v1/users/facebook-api.js
@@ -6,6 +6,7 @@ const facebookEntryService = (fbData,fbUrl) => {
     return new Promise((resolve, reject) => {
         if(!isEmpty(fbData)){
             const bData = JSON.parse(JSON.stringify(fbData));
+            console.log(bData);
             var options = {
                 "method": "POST",
                 "url": "https://graph.facebook.com/v11.0/560345878459615/events?access_token=EAAHLiM2ETPEBAPK53iQgv6l5AIbZBoMkulrqUsai2StS32TafDN2Kr3mq8ZCktLvPNFK12tgZAjPHZAGC6ZAbTcHCUho5ZCTu6TtP3iZAbRqEesXuxw9kEZAkzsFCwHPESBbaGSZBQ07sf2bSgbNrnq7Dj1k54VbPYMZBuhodbtzQvoEOU4FCUe9PPIhi0pwZAJQvYZD", 
