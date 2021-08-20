2aa7054711a16dc95b9a03ea07a03fc4bdea5651
shashireal11 <>
2021-08-20 14:31:00 +0530

    Update user-google-signup.js

--- server/api/v1/users/user-google-signup.js
+++ server/api/v1/users/user-google-signup.js
@@ -611,7 +611,7 @@ async function getUserName()
 {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for( var i=0; i < 15; i++ )
+    for( var i=0; i < 11; i++ )
         text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text;
 }
