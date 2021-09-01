f18fb2276d52d16c39c21db9e21d362957df0f24
shashireal11 <>
2021-09-01 15:42:56 +0530

    show preview off

--- server/api/v1/contest/contest-detail.js
+++ server/api/v1/contest/contest-detail.js
@@ -719,7 +719,7 @@ module.exports = {
                     join_multiple_teams: multipleTeam,
                     is_gadget: gadgetLeague,
                     total_winners: (contestDetail.breakup && contestDetail.breakup.length > 0) ? contestDetail.breakup.pop() : {}, //toalWinner,
-                    teams_joined: (reviewMatch && reviewMatch.is_parent && sport == 1 && reviewMatch.show_preview == 0 && (reviewMatch.match_status == 'In Progress' || reviewMatch.match_status == 'Not Started'))? 0 :joinedTeams,
+                    teams_joined: (reviewMatch && reviewMatch.is_parent && sport == 8 && reviewMatch.show_preview == 0 && (reviewMatch.match_status == 'In Progress' || reviewMatch.match_status == 'Not Started'))? 0 :joinedTeams,
                     is_joined: is_joined,
                     my_team_ids: myTeamIds,
                     joined_team_list: teamRankData,
