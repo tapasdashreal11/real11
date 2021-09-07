5c525129efd09da312ae4e159ff6da85e0664942
shashireal11 <>
2021-09-07 16:31:24 +0530

    Update week-leaderboard-series-api.js

--- server/api/v1/users/week-leaderboard-series-api.js
+++ server/api/v1/users/week-leaderboard-series-api.js
@@ -17,6 +17,7 @@ module.exports = {
                  response["data"] = data;
                  response["message"] = "";
                  response["status"] = true;
+                 return res.json(response);
             } else {
                 let seriesData = await Series.find({$or:[{week_leaderboard:1},{series_leaderboard:1}]});
                 if(seriesData && seriesData.length>0){
