485c28617872082e524c5fca23701a78f7261ce4
shashireal11 <>
2021-09-16 13:05:41 +0530

    Update facebook-api.js

--- server/api/v1/users/facebook-api.js
+++ server/api/v1/users/facebook-api.js
@@ -15,6 +15,7 @@ const facebookEntryService = (fbData,fbUrl) => {
                 "body":bData
             };
             request(options, function (error,res,body) {
+                console.log('body',body);
                 const fdData = fbUrl ? JSON.parse(JSON.stringify(fbUrl)) : {};
                 UserFbTrack.create({fbtrace_id:body.fbtrace_id,events_received:body.events_received,events_obj:fdData});
                 return resolve(body);
