e95bc6259c2c3cd29bf877f91fdc68595214b453
shashireal11 <>
2021-09-16 15:04:38 +0530

    Update private-contest-create.js

--- server/api/v1/contest/private-contest-create.js
+++ server/api/v1/contest/private-contest-create.js
@@ -48,7 +48,7 @@ module.exports = {
             contest_size = parseInt(contest_size);
             winners_count = parseInt(winners_count);
             // create contest invite code
-            return res.send(ApiUtility.failed('Temporary stop this activity.'));
+           // return res.send(ApiUtility.failed('Temporary stop this activity.'));
             let inviteCode = Helper.createUserReferal(6);
             let decoded = {
                 contest_size,
