8ea157c0753cf2264bd54b2418a5708c0d51262b
shashireal11 <>
2021-09-16 13:40:13 +0530

    Update users-verify-otp.js

--- server/api/v1/users/users-verify-otp.js
+++ server/api/v1/users/users-verify-otp.js
@@ -234,6 +234,7 @@ async function setFacebookEventAtSingup(params,userIp){
 						"ph": params && params.temp_phone ? sha256(params.temp_phone) : null,
 						"fbc": params && params.fbc_id ? params.fbc_id : null,
 						"fn": params && params.team_name ? sha256(params.team_name) : null,
+						"external_id":params && params._id ? params._id:null,
 						"client_ip_address": userIp ? userIp: "172.17.0.5",
 						"client_user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
 					},
