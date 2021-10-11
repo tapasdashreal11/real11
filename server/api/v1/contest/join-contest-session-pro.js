535c8e30e319329cc5ddedff0f725a7d0918d70e
shashireal11 <>
2021-10-11 13:04:32 +0530

    Update join-contest-session-pro.js

--- server/api/v1/contest/join-contest-session-pro.js
+++ server/api/v1/contest/join-contest-session-pro.js
@@ -1113,7 +1113,7 @@ async function joinContestPaymentCalculation(contest_size,offerableAppled, useab
     if (remainingFee) {
         let extraBalance = authUser.extra_amount || 0;
         let extraBal = 0;
-        if (extraBalance && extraBalance > 0 && contest_size > 25 && !authUser.xtra_cash_block) {
+        if (extraBalance && extraBalance > 0 && contest_size > 25 && (authUser && !authUser.xtra_cash_block) ) {
             let perDayExtraAmt = 0;
             let perDayLimit = config.extra_bonus_perday_limit;
             if (String(authUser.extra_amount_date) == String(indianDate)) {
