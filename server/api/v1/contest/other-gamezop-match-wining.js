19f0a034329e5bdc97b90c02d172fd9c3d49afff
shashireal11 <>
2021-10-25 18:04:09 +0530

    Update other-gamezop-match-wining.js

--- server/api/v1/contest/other-gamezop-match-wining.js
+++ server/api/v1/contest/other-gamezop-match-wining.js
@@ -9,14 +9,14 @@ const { Validator } = require("node-input-validator");
 module.exports = async (req, res) => {
     try {
         const {room_id} = req.params;
-        const user_id = req.userId;
+        //const user_id = req.userId;
         let constraints = { room_id: "required"};
         let validator = new Validator(req.params, constraints);
         let matched = await validator.check();
         if (!matched) {
             return res.send(ApiUtility.failed('Parameters are not properly !!'));
         }
-        if(roomId && user_id){
+        if(room_id){
             let playerTeamRes = await OtherGamesPtc.find({contest_id:ObjectId(room_id),is_deleted:0,winning_amount_distributed:1});
             let winuserList = playerTeamRes ? playerTeamRes : [];
             var finalResult = ApiUtility.success(winuserList);
