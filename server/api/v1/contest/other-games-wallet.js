a4cae25be9f050086368ef257d9945ea2fb5b946
shashireal11 <>
2021-12-09 16:37:55 +0530

    Update other-games-wallet.js

--- server/api/v1/contest/other-games-wallet.js
+++ server/api/v1/contest/other-games-wallet.js
@@ -33,7 +33,10 @@ module.exports = {
             let is_offer_applied = false;
             let couponSaleData = [];
             // //////console.log(req.userId);
-            let userdata = await User.findOne({ _id: decoded['user_id'] })
+            let userdata = await User.findOne({ _id: decoded['user_id'] });
+            if(userdata && userdata.fair_play_violation && userdata.fair_play_violation ==1){
+                return res.send(ApiUtility.failed("You can't join contest.You are under fair play violation!!"));
+            }
             if (userdata) {
                 adminPer = 0; //(setting.admin_percentage) ? setting.admin_percentage : 0;
                 let useableBonusPer = adminPer;
