136db3dda46e1dff50adae50424806959d88c939
shashireal11 <>
2021-12-20 18:52:32 +0530

    otp msz

--- server/api/v1/users/users-login.api.js
+++ server/api/v1/users/users-login.api.js
@@ -50,7 +50,7 @@ module.exports = async (req, res) => {
 						logger.error("MSG_ERROR", err.message);
 					});
 				
-				let mailMessage	=	"<div><h3>OTP Request</h3><p>Hi,</p><p>You One Time Password(OTP) is <b>"+ otp +"</b></p><p>The password will expire in 10 minnutes if not used.</p><p>If you have not made this request, please contact our customer support immidiately.</p><br/ ><p>Thank You,</p><p>Real11 Team</p></div>"
+				let mailMessage	=	"<div><h3>OTP Request</h3><p>Hi,</p><p>Your One Time Password(OTP) is <b>"+ otp +"</b></p><p>The password will expire in 10 minutes if not used.</p><p>If you have not made this request, please contact our customer support immidiately.</p><br/ ><p>Thank You,</p><p>Real11 Team</p></div>"
 				let to	=	data.email;
 				let subject	=	"One Time Password (OTP) login to Real11";
 				
