d56b0716bdefebabd2c4853f2376db41e1f52515
shashireal11 <>
2021-12-24 11:30:07 +0530

    Update other-gamezop-match.js

--- server/api/v1/contest/other-gamezop-match.js
+++ server/api/v1/contest/other-gamezop-match.js
@@ -35,13 +35,13 @@ module.exports = {
                                 // Solitaire Gold
                                 let itemObj = o;
                                 itemObj.match_id = 112;
-                                itemObj.match_img = gamesMatch && gamesMatch.length >0 && gamesMatch[0].match_logo ? imageurl+"/"+gamesMatch[0].match_logo :'';
+                                itemObj.match_img = gamesMatch && gamesMatch.length >0 && gamesMatch[0].match_logo ? imageurl+"/"+gamesMatch[1].match_logo :'';
                                 return itemObj;
                             }else if (_.isEqual(o.code, "H1PJn6mqAr")) {
                                 // Knife Flip
                                 let itemObj = o;
                                 itemObj.match_id = 113;
-                                itemObj.match_img = gamesMatch && gamesMatch.length >0 && gamesMatch[0].match_logo ? imageurl+"/"+gamesMatch[0].match_logo :'';
+                                itemObj.match_img = gamesMatch && gamesMatch.length >0 && gamesMatch[0].match_logo ? imageurl+"/"+gamesMatch[2].match_logo :'';
                                 return itemObj;
                             }
                         }
