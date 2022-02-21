0b63c81523fe49839edda3998243909883c5ec5d
shashireal11 <>
2022-02-21 15:13:59 +0530

    contest for champ

--- server/api/ModelService.js
+++ server/api/ModelService.js
@@ -232,7 +232,8 @@ class ModelService {
                                         "is_favourite":false,
                                         "maximum_team_size": {$cond: { if: { $in: [ "$$sec.contest.multiple_team", ["yes",true] ] }, then: { $cond: { if: { $ifNull: ["$$sec.contest.maximum_team_size",false] },then: "$$sec.contest.maximum_team_size",else: 9 } }, else: 1 }},
                                         "contest_shareable": {$cond: { if: { $ifNull: [ "$$sec.contest.contest_shareable", false ] }, then: "$$sec.contest.contest_shareable", else: 0 }},
-                                        "contest_comment":{ $ifNull: [ "$$sec.contest_comment", "" ] }
+                                        "contest_comment":{ $ifNull: [ "$$sec.contest_comment", "" ] },
+                                        "champ_type":{ $ifNull: [ "$$sec.champ_type", 0 ] }
                                     }
                                 }
                             },
