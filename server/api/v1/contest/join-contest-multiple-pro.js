6f8739b1654ad7d840bbd3a1c0d0baa5042cbb73
shashireal11 <>
2022-02-23 12:43:43 +0530

    Update join-contest-multiple-pro.js

--- server/api/v1/contest/join-contest-multiple-pro.js
+++ server/api/v1/contest/join-contest-multiple-pro.js
@@ -307,6 +307,7 @@ module.exports = async (req, res) => {
                                                             if(matchContest && matchContest.is_offerable){
                                                                 let totalJoinedTeam = joinedContestWithTeamCounts;
                                                                 let calJoinTeam = total_team_number + totalJoinedTeam;
+                                                                console.log("calJoinTeam**",calJoinTeam,totalJoinedTeam);
                                                                 if(matchContest.offer_after_join >= totalJoinedTeam && calJoinTeam > matchContest.offer_after_join && matchContest.offerable_amount > 0){
                                                                     if(calEntryFees > 0){
                                                                         offerableAppled = true;
@@ -315,6 +316,7 @@ module.exports = async (req, res) => {
                                                                         let offerdAmount = matchContest.offerable_amount >= recalcalEntryFees ? recalcalEntryFees: matchContest.offerable_amount;
                                                                         let totalOfferdAmount = retention_bonus_amount + offerdAmount;
                                                                         retention_bonus_amount = totalOfferdAmount;
+                                                                        console.log("retention_bonus_amount",retention_bonus_amount);
                                                                     }
                                                                  } 
                                                               }
