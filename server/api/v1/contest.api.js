1b8f25ceb70835ae8336d8fbcaeedf3d1fac8440
shashireal11 <>
2022-02-23 11:56:26 +0530

    Update contest.api.js

--- server/api/v1/contest.api.js
+++ server/api/v1/contest.api.js
@@ -658,14 +658,14 @@ module.exports = {
                             
                             let totalJoinedTeam = await PlayerTeamContest.find({ 'contest_id': contest_id, 'user_id': decoded['user_id'], 'match_id': decoded['match_id'], 'sport': match_sport, 'series_id': decoded['series_id'] }).countDocuments();
                             if ((matchContestData && matchContestData.is_auto_create) || (matchContestData && matchContestData.contest && matchContestData.contest.is_auto_create) ) {
-                                
+                                console.log("Autocreeta",matchContestData.is_auto_create);
                                  let mParentId = matchContestData && matchContestData.parent_contest_id ? matchContestData.parent_contest_id :matchContestData.contest_id;
                                  totalJoinedTeam = await PlayerTeamContest.find({ 'parent_contest_id': mParentId, 'user_id': decoded['user_id'], 'match_id': decoded['match_id'], 'sport': match_sport }).countDocuments();
                             }
                             let calJoinTeam = total_team_number + totalJoinedTeam;
                             if(matchContestData.offer_after_join >= totalJoinedTeam && calJoinTeam > matchContestData.offer_after_join && matchContestData.offerable_amount > 0){
                                 if(calEntryFees > 0){
-                                   // console.log('in***');
+                                    console.log('in***');
                                     offerableAppled = true;
                                     let recalcalEntryFees = calEntryFees;
                                     calEntryFees = matchContestData.offerable_amount >= calEntryFees ? 0: (calEntryFees - matchContestData.offerable_amount );
