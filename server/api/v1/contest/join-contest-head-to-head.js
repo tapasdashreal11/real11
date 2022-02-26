5d30f1e127aa6ea93dc433ec19a1bd3b6103ae8a
shashireal11 <>
2022-02-26 10:59:27 +0530

    Update join-contest-head-to-head.js

--- server/api/v1/contest/join-contest-head-to-head.js
+++ server/api/v1/contest/join-contest-head-to-head.js
@@ -334,7 +334,7 @@ module.exports = async (req, res) => {
                                                                     let calJoinTeam = 1 + totalJoinedTeam;
                                                                     if (matchContest.offer_after_join >= totalJoinedTeam && calJoinTeam > matchContest.offer_after_join && matchContest.offerable_amount > 0) {
                                                                         if (calEntryFees > 0) {
-                                                                            offerableAppled = true;
+                                                                            offerableAppled = false;
                                                                             let recalcalEntryFees = calEntryFees;
                                                                             calEntryFees = matchContest.offerable_amount >= calEntryFees ? 0 : (calEntryFees - matchContest.offerable_amount);
                                                                             let offerdAmount = matchContest.offerable_amount >= recalcalEntryFees ? recalcalEntryFees : matchContest.offerable_amount;
@@ -1066,7 +1066,7 @@ async function joinContestGlobal(res, refer_by_user, refer_code, joinedContestCo
                         let calJoinTeam = 1 + totalJoinedTeam;
                         if (matchContest.offer_after_join >= totalJoinedTeam && calJoinTeam > matchContest.offer_after_join && matchContest.offerable_amount > 0) {
                             if (calEntryFees > 0) {
-                                offerableAppled = true;
+                                offerableAppled = false;
                                 let recalcalEntryFees = calEntryFees;
                                 calEntryFees = matchContest.offerable_amount >= calEntryFees ? 0 : (calEntryFees - matchContest.offerable_amount);
                                 let offerdAmount = matchContest.offerable_amount >= recalcalEntryFees ? recalcalEntryFees : matchContest.offerable_amount;
