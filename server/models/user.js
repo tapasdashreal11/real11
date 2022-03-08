a224d3a1ec2098a1d133ccf918ecda66c26c59e8
nidhimittal16 <65557308+nidhimittal16@users.noreply.github.com>
2022-03-08 17:49:47 +0530

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -134,6 +134,7 @@ const AdminSchema = mongoose.Schema({
     xtra_cash_block: { type: Number, enum: [1,0], default: 0 },
     bonus_amount_block: { type: Number, enum: [1,0], default: 0 },
     bank_request_date: {type: Date},
+    bank_xtra_amount: {type: Number, default: 0},
     win_dis_status: { type: Boolean, default: false}
 }, {
         timestamps: { createdAt: 'created', updatedAt: 'modified' },
