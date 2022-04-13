9ea172530273b4c54b1051eb891d37be5386b9e5
shashireal11 <>
2022-04-13 12:56:53 +0530

    Update users-verify-otp.js

--- server/api/v1/users/users-verify-otp.js
+++ server/api/v1/users/users-verify-otp.js
@@ -269,8 +269,6 @@ async function setDataToAppsflyer(params){
                 'advertising_id': params && params.user_gaid ? params.user_gaid : '',
 				"eventValue": JSON.stringify(event_val)
 			};
-			console.log("appsflyerURL***",appsflyerURL);
-			console.log("eName***",eName);
 			appsFlyerEntryService(signUpBody, appsflyerURL);
 			// This code is block till IPL
            /* if (!params.is_refered_by) {
