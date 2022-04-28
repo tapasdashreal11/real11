b0010e074510503d8efd21f3be1a981623f35314
nidhimittal16 <65557308+nidhimittal16@users.noreply.github.com>
2022-04-28 11:26:52 +0530

    cashfree transaction and webhhok

--- server/router.js
+++ server/router.js
@@ -506,22 +506,19 @@ router.post('/mobikwik/webhook', function(req, res) {
 });
 
 router.post('/cashfree/webhook', function(req, res) {
-    console.log("cashfree callback data", req.body, req.body.data.payment.payment_method)
-    console.log("cashfree callback data type", typeof req.body, JSON.stringify(req.body))
+    console.log("cashfree callback data", req.body)
+    // console.log("cashfree callback data type", typeof req.body )
     console.log("cashfree callback header", req.headers);
     if(req.body && req.body.data) {
         let bodyRes =   req.body.data;
-        // if (bodyRes && bodyRes.order && bodyRes.order.order_id !== "" && bodyRes.payment && bodyRes.payment.payment_status == "SUCCESS"  ) {
-        //     // console.log('zsxdcfvbnb');
-        //     updateTransactionCashfreeWebhook(bodyRes, req.body, req.headers, 'CASH_FREE', function(resResult) {
-        //         return res.send(resResult)
-        //     });
-        // }
+        if (bodyRes && bodyRes.order && bodyRes.order.order_id !== "" && bodyRes.payment && bodyRes.payment.payment_status == "SUCCESS"  ) {
+            updateTransactionCashfreeWebhook(bodyRes, req.body, req.headers, 'CASH_FREE', function(resResult) {
+                return res.send(resResult)
+            });
+        }
     } else {
         return res.send({ status: false, "message":"Something went wrong..!!" });
     }
-
-    return res.send({ status: 'success' });
 });
 
 
