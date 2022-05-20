82a5c74335b755e5a7aff6b03d14477deef7ce74
shashireal11 <>
2022-05-20 16:30:28 +0530

    Update other-games-wallet.js

--- server/api/v1/contest/other-games-wallet.js
+++ server/api/v1/contest/other-games-wallet.js
@@ -84,7 +84,7 @@ module.exports = {
                         let calEntryFees = entryFee;
                         try {
                             redis.getRedisForUserAnaysis(redisKeyForRentation, async (err, rdata) => {
-                                const ludoOffer = await LudoOffer.findOne({user_id:ObjectId(req.userId),match_id:parseInt(match_id),status: 1 });
+                                const ludoOffer = await LudoOffer.findOne({});
                                 console.log(ludoOffer);
                                 if(ludoOffer && ludoOffer._id){
                                     let cBonus =  ludoOffer && ludoOffer.contest_bonous?ludoOffer.contest_bonous:[]; 
