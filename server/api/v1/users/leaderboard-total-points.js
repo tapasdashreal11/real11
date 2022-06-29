1770218fc6e592fa93eec509dbfb4bc32d3a4260
shashireal11 <>
2022-06-29 16:17:14 +0530

    user_id added

--- server/api/v1/users/leaderboard-total-points.js
+++ server/api/v1/users/leaderboard-total-points.js
@@ -9,6 +9,38 @@ module.exports = {
     megaLeaderBoardTotalPointsCal: async (req, res) => {
         var response = { status: false, message: "Invalid Request", data: {} };
         try {
+           let {s_id,user_id} = req.params; 
+           let redisKeyForSeriesWeekBoardMeta = 'mega-lb-total-points-';
+           redis.getRedisWeekLeaderboard(redisKeyForSeriesWeekBoardMeta, async (err, data) => {
+            if (data) {
+                 response["data"] = data;
+                 response["message"] = "";
+                 response["status"] = true;
+                 return res.json(response);
+            } else {
+                let seriesData = await Series.find({mega_leaderboard:1,id_api:s_id});
+                if(seriesData && seriesData.length>0){
+                let data = await MeagaLbTotalPoint.find({series_id:s_id,user_id:user_id});
+                 // consoleredis.setRedisWeekLeaderboard(redisKeyForSeriesWeekBoardMeta, newSeriesData);
+                 response["data"] = data ? data : [];
+                 response["message"] = "";
+                 response["status"] = true;
+                } else {
+                 response["data"] = [];
+                 response["message"] = "No Data found!!";
+                 response["status"] = false;
+                }
+                return res.json(response);
+            }
+           });
+        } catch (err) {
+            response["msg"] = err.message;
+            return res.json(response);
+        }
+    },
+    megaLeaderBoardTotalPointsCal1: async (req, res) => {
+        var response = { status: false, message: "Invalid Request", data: {} };
+        try {
             let {s_id} = req.params; 
             let userId = req.userId;
            let redisKeyForSeriesWeekBoardMeta = 'mega-lb-total-points-';
