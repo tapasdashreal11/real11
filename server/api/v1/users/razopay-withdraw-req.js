1a3a0ec398fe59617a3dc26e218f50fcdb8380d4
shashireal11 <>
2022-08-23 11:48:11 +0530

    Update razopay-withdraw-req.js

--- server/api/v1/users/razopay-withdraw-req.js
+++ server/api/v1/users/razopay-withdraw-req.js
@@ -46,7 +46,7 @@ module.exports = async (req, res) => {
 			return res.json(response);
 		}
 		try {
-			let userId =req.userId;
+			let userId ="6171105acd54863bd0c02c17" //req.userId;
 			let user = await Users.findOne({ _id: userId, fair_play_violation: 0 });
 			let userRazopayData = await UserRazopayFundAc.findOne({ user_id: userId });
 			if (userRazopayData && userRazopayData.contact_id && userRazopayData.fund_account_id) {
@@ -147,6 +147,9 @@ module.exports = async (req, res) => {
 								let settingData = {};
 								let appSettingData = {};
 								redis.getRedis('app-setting', async (err, data) => {
+									let todayStartDate = moment().startOf('day').utc().toDate();
+									let totalUserReqOnToday = await WithdrawRequest.find({user_id:ObjectId(userId),created:{$gte:todayStartDate}}).count();
+									console.log("totalUserReqOnToday",todayStartDate,totalUserReqOnToday);
 									if (data) {
 										settingData = data;
 									} else {
@@ -158,8 +161,7 @@ module.exports = async (req, res) => {
 										return res.json(response);
 									}
 									if (params.instant_withdraw && params.instant_withdraw == "1") {
-										let todayStartDate = moment().startOf('day').utc().toDate();
-									    let totalUserReqOnToday = await WithdrawRequest.find({user_id:ObjectId(userId),created:{$gte:todayStartDate}}).count();
+										
 									    
 										let instantComm = 0;
 										if (params.type == "bank" && totalUserReqOnToday >2) {
