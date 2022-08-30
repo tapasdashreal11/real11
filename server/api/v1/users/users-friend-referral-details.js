a990f7f4997eb63998d67daf18b3a18bb130c838
nidhimittal16 <65557308+nidhimittal16@users.noreply.github.com>
2022-08-30 16:38:02 +0530

    Update users-friend-referral-details.js

--- server/api/v1/users/users-friend-referral-details.js
+++ server/api/v1/users/users-friend-referral-details.js
@@ -5,8 +5,7 @@ const { Validator } = require("node-input-validator");
 const ApiUtility = require("../../api.utility");
 const logger = require("../../../../utils/logger")(module);
 
-// @response
-// {"response":{"status":true,"message":null,"data":{"total_earnd":0,"to_be_earnd":0,"total_fields":0,"friend_detail":[]}}}
+
 
 
   module.exports = async (req, res) => {
