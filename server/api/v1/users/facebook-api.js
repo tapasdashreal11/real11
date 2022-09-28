551b391d96f2e1bd62e8625d5645d046327ef63f
nidhimittal16 <65557308+nidhimittal16@users.noreply.github.com>
2022-09-28 17:48:44 +0530

    Update facebook-api.js

--- server/api/v1/users/facebook-api.js
+++ server/api/v1/users/facebook-api.js
@@ -9,7 +9,8 @@ const facebookEntryService = (fbData,fbUrl) => {
             const bData = JSON.parse(JSON.stringify(fbData));
             var options = {
                 "method": "POST",
-                "url": "https://graph.facebook.com/v11.0/560345878459615/events?access_token=EAAHLiM2ETPEBAPK53iQgv6l5AIbZBoMkulrqUsai2StS32TafDN2Kr3mq8ZCktLvPNFK12tgZAjPHZAGC6ZAbTcHCUho5ZCTu6TtP3iZAbRqEesXuxw9kEZAkzsFCwHPESBbaGSZBQ07sf2bSgbNrnq7Dj1k54VbPYMZBuhodbtzQvoEOU4FCUe9PPIhi0pwZAJQvYZD", 
+                // "url": "https://graph.facebook.com/v11.0/560345878459615/events?access_token=EAAHLiM2ETPEBAPK53iQgv6l5AIbZBoMkulrqUsai2StS32TafDN2Kr3mq8ZCktLvPNFK12tgZAjPHZAGC6ZAbTcHCUho5ZCTu6TtP3iZAbRqEesXuxw9kEZAkzsFCwHPESBbaGSZBQ07sf2bSgbNrnq7Dj1k54VbPYMZBuhodbtzQvoEOU4FCUe9PPIhi0pwZAJQvYZD", 
+                "url": "https://graph.facebook.com/v11.0/560345878459615/events?access_token=EAAHLiM2ETPEBAC2tOlgnNqayW9zwOGBiiFJUGye3fUADYtzwQs7MGNYa9vafTPEE8CQdhvtUT8T3fEQdHaWR3qilHJmLQ936hwfDTQaFiGgtyeHiGDlVx1ZAQdbWOzAzjrvYHRiDjP5CZAgClZAz0ldfo3AZCNHDc4kvncHWwjXyv3gxb0ZBwohMZBgnO71ssZD", 
                 "json": true,
                 "headers": {'Content-Type': 'application/json'},
                 "body":bData
