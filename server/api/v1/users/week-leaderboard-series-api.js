a9bd6929cc9ba7b46f8fe7f5a053608ce97df5da
nidhimittal16 <65557308+nidhimittal16@users.noreply.github.com>
2022-10-10 14:29:49 +0530

    Update week-leaderboard-series-api.js

--- server/api/v1/users/week-leaderboard-series-api.js
+++ server/api/v1/users/week-leaderboard-series-api.js
@@ -19,7 +19,7 @@ module.exports = {
                  response["status"] = true;
                  return res.json(response);
             } else {
-                let seriesData = await Series.find({$or:[{week_leaderboard:1},{series_leaderboard:1},{mega_leaderboard:1}]});
+                let seriesData = await Series.find({$or:[{week_leaderboard:1},{series_leaderboard:1},{mega_leaderboard:1}]}).sort({ start_date: -1 });
                 if(seriesData && seriesData.length>0){
                  let newSeriesData = seriesData.map((val)=>{
                      let return_data = JSON.parse(JSON.stringify(val));
