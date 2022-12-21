fcb760b085b254606fbdce5a71aa37b15a24131f
gayas <85106698+gayasreal11@users.noreply.github.com>
2022-12-21 10:50:21 +0530

    update join-contest-wallet-amount-multple

--- server/api/v1/contest.api.js
+++ server/api/v1/contest.api.js
@@ -749,9 +749,10 @@ module.exports = {
                         } 
                         let liveMatch = await SeriesSquad.findOne({ 'match_id': decoded['match_id'], 'sport': match_sport, 'series_id': decoded['series_id'] });
                         if(liveMatch.is_parent == false && liveMatch.live_fantasy_parent_id && (matchContestData.category_slug == "last-man-standing" || matchContestData.category_slug == "head-to-head")) {
+                            console.log("Console is here in if .............");
                             usableAmt 	=	0;
                         }
-                        
+
                         data['cash_balance'] = (cashBalance) ? cashBalance : 0;
                         data['winning_balance'] = (winningBalance) ? winningBalance : 0;
                         data['usable_bonus'] = usableAmt ? parseFloat(usableAmt.toFixed(2)) : 0;
